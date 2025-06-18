edc207dd3507b0605cc54260081014fbc5ba8f59
VigneshrajNadar <vigneshrajnadar@gmail.com>
2025-06-18 20:18:02 +0530

    Fix deployment configuration

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Set Python path
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate 
