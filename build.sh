2c0430fd9bebcc7f5d13a9c0890904e59ce868c3
VigneshrajNadar <vigneshrajnadar@gmail.com>
2025-06-18 20:27:45 +0530

    Update Python version and dependencies for Python 3.13

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 set -o errexit
 
 # Upgrade pip
-python -m pip install --upgrade pip
+python3.13 -m pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Set environment variables
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src
+export PYTHONPATH=/opt/render/project/src
 export DJANGO_SETTINGS_MODULE=college_management_system.settings
 
 # Collect static files
